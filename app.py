e7e3d1b6f56ba0de4eaa80b85a121137fd567ed4
Axnusic <shresprbn@gmail.com>
2023-05-01 20:26:20 +0545

    changes

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, request
 import joblib
 import numpy as np
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
 
 app = Flask(__name__)
 
@@ -25,22 +26,28 @@ def predict():
 
     data = {
         'battery_power' : [battery_pow],
-        'dual_sim' : [dual_sim],
         'int_memory' : [int_mem],
         'px_height' : [px_height],
         'px_width' : [px_width],
         'ram' : [RAM],
-        'four_g': [four_g],
         'sc_h' : [screen_height],
         'sc_w' : [screen_width],
+        'dual_sim' : [dual_sim],
+        'four_g': [four_g],
         'wifi' : [wifi],
     }
-    data1 = [[battery_pow],[int_mem],[px_height],[px_width],[RAM],[screen_height],[screen_width],[dual_sim],[four_g],[wifi]]
     df = pd.DataFrame(data)
-    print(df)
+    scaler = StandardScaler()
+    data1 = [[battery_pow],[int_mem],[px_height],[px_width],[RAM],[screen_height],[screen_width],[dual_sim],[four_g],[wifi]]
+    data2 = scaler.fit_transform(df)
+    data2 = np.array(data2)
+    # dataScaled=np.concatenate((num_data,cat_data1), axis = 1)
+    # dataScaled = dataScaled.tolist()
+    data2 = data2.tolist()
+    print(RAM)
     model = joblib.load("model.pkl")
 
-    Y_pred = (model.predict(df))
+    Y_pred = (model.predict(data2))
     print(Y_pred)
 
     return render_template("slider.html", my_prediction = Y_pred)
